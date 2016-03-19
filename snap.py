dbf5ec34484a0ccdfe91a1f219d29e8f572cd402
AugustusZ <augustus.z@outlook.com>
2016-03-18 21:51:44 -0700

    add email-sending

--- snap.py
+++ snap.py
@@ -5,6 +5,12 @@ HOST = "yankuanz@aludra.usc.edu"
 DEST_PATH = "/home/scf-09/yankuanz/public_html/snap/latest.jpg"
 SCRIPT_DIR = "/Users/Augustus/Documents/snap"
 ENHENCED_IMG_NAME = "enhenced.jpg"
+CROPPED_IMG_NAME = "cropped.jpg"
+SENT_IMG_NAME = "LatestSnap.jpg"
+RECEIVER = "yankuanz@usc.edu"
+SUBJECT = "This is my latest selfie!"
+RUNNING_LIMIT = 6 * 60 * 24 * 7 # one week
+
 # get the most recent file name from SRC_DIR
 def getLatestImgName():
 	mtime = lambda f: os.stat(os.path.join(SRC_DIR, f)).st_mtime
@@ -18,17 +24,19 @@ def getLatestImgName():
 
 # upload to file to server and then delete it locally
 def copyToServer(fileToCopy):
-	os.system('scp "%s" "%s:%s"' % (os.path.join(SRC_DIR, fileToCopy), HOST, DEST_PATH))
+	print "Upload file:", os.path.join(SCRIPT_DIR, fileToCopy)
+	os.system('scp "%s" "%s:%s"' % (os.path.join(SCRIPT_DIR, fileToCopy), HOST, DEST_PATH))
+	time.sleep(1)
 	# delete the file from local after uploading it
 	# otherwise, it will mess up with getLatestImgName()
 	# print fileToCopy
 	# if fileToCopy == "upload":
-	time.sleep(3)
-	os.system('rm "%s"' % (os.path.join(SRC_DIR, fileToCopy)))
+	# os.system('rm "%s"' % (os.path.join(SCRIPT_DIR, fileToCopy)))
 
 # enhence image before upload it and return new filename if any
 def enhenceImage(imageFileName):
 	# 1. read in as matrix and crop it to square (720x720)
+	print "Process:", os.path.join(SRC_DIR, imageFileName)
 	img = cv2.imread(os.path.join(SRC_DIR, imageFileName))
 	img = img[:, 280:1000, :] #crop in the middle
 
@@ -44,23 +52,41 @@ def enhenceImage(imageFileName):
 	if len(faces) > 0:
 		for i,(x,y,w,h) in enumerate(faces):
 			cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
-		# 4. save it
-	cv2.imwrite(os.path.join(SRC_DIR, ENHENCED_IMG_NAME), img)
+	
+	# 4. save it
+	print "Save to:", os.path.join(SCRIPT_DIR, ENHENCED_IMG_NAME)
+	cv2.imwrite(os.path.join(SCRIPT_DIR, ENHENCED_IMG_NAME), img) # for webpage
+	cv2.imwrite(os.path.join(SCRIPT_DIR, CROPPED_IMG_NAME), img) # for email
+	time.sleep(1)
+
+# email original image
+def sendEmail(imageFileName):
+	# os.system('uuencode %s "%s" | mail -s "%s" %s' % (os.path.join(SRC_DIR, imageFileName), imageFileName, SUBJECT, RECEIVER))
+	print "Email", os.path.join(SCRIPT_DIR, imageFileName), "to", RECEIVER
+	os.system('(echo "%s"; uuencode %s "%s") | mail -s "%s" %s' % ("This snap is taken at " + time.strftime('%X %x') + '.\n', os.path.join(SCRIPT_DIR, imageFileName), imageFileName, SUBJECT, RECEIVER))  
+	# os.system('(echo %s; uuencode %s "%s") | mail -s "%s" %s' % ("This snap is taken at " + time.strftime('%X %x'), os.path.join(SCRIPT_DIR, imageFileName), "snap.jpg", SUBJECT, RECEIVER))  
 
 def sendIfUpdated(originalFile):
 	latest = getLatestImgName()
 	if not latest or latest == originalFile: 
+		print "[ Nothing new ]"
 		return False
+	print "[ Something new ]", latest
 	enhenceImage(latest)
+	sendEmail(CROPPED_IMG_NAME)
 	copyToServer(ENHENCED_IMG_NAME)
 	return latest
 
 lastFile = None
-while True:
+count = 0
+while count < RUNNING_LIMIT:
 	result = sendIfUpdated(lastFile)
 	if result: 
 		# print("Uploaded file: {}".format(result))
 		lastFile = result
 	# else:
 	# 	print("Nothing new. Latest: {}".format(lastFile))
-	time.sleep(5)
+	time.sleep(10)
+	count += 1
+	print "[ Progress ] {0:.3f}%".format(count / (RUNNING_LIMIT + 0.1) * 100)
+	print 
